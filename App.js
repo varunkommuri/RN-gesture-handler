6d255b4db8a60fb59969612f486966d099812568
Kommuri venkata varun <meetvarun.kommuri@gmail.com>
2021-01-27 21:09:25 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,Animated } from 'react-native';
+import GestureHanlder,{PinchGestureHandler,State} from 'react-native-gesture-handler'
 
 export default function App() {
+  let scale = new Animated.Value(1);
+  onPinchEvent = Animated.event(
+    [
+      {
+        nativeEvent: { scale:scale }
+      }
+    ],
+    {
+      useNativeDriver: true
+    }
+  )
+  onPinchStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      Animated.spring(scale, {
+        toValue: scale,
+        useNativeDriver: true
+      }).start()
+      /* Animated.event(
+        [
+          {
+            nativeEvent: { scale:scale }
+          }
+        ],
+        {
+          useNativeDriver: true
+        }
+      ) */
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <PinchGestureHandler
+      onGestureEvent={onPinchEvent}
+      onHandlerStateChange={onPinchStateChange}
+    
+    >
+      <Animated.Image
+      source={{uri:'https://admin.hksinc.com/wp-content/uploads/2018/09/SanFrancisco_01.jpg'}}
+      style={{height:400,width:400,transform:[{scale: scale}]}}
+      resizeMode="contain"
+
+      />
+      </PinchGestureHandler>
       <StatusBar style="auto" />
     </View>
   );
